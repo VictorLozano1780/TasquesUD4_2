811bed75f7fe71a856f79c8d504ae34bd00135f2
Víctor Lozano <55434666+VictorLozano1780@users.noreply.github.com>
2021-02-02 16:36:43 +0100

    Add files via upload

---
+++ TasquesUD4_2/src/TasquesUD4_2.java
@@ -0,0 +1,19 @@
+
+public class TasquesUD4_2 {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n = 5;
+		double a = 4.56;
+		char c = 'a';
+		int ascii = c;
+		
+		System.out.println("n= " + n);
+		System.out.println("a= " + a);
+		System.out.println("c= " + c);		
+		System.out.println("n+a= " + (n + a));
+		System.out.println("a-n= " + (a - n));
+		System.out.println("Valor numerico del caracter"+ c + " = " + ascii);
+		
+		
+	}
+}
